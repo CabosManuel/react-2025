659f5effebe5db9ed1f52c4483f547f2e06b8c35
CabosManuel <cabos.apolaya@gmail.com>
2025-03-18 19:23:08 -0500

    useState with ToggleButton

---
+++ my-react-app-state/components/ToggleButton.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const ToggleButton = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <button onClick={ () => setIsActive(!isActive) }>
+      { isActive ? '👋😄' : '💤😞' }
+    </button>
+  )
+}
+
+export default ToggleButton
--- my-react-app-state/src/App.jsx
+++ my-react-app-state/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Counter from '../components/Counter'
+import ToggleButton from '../components/ToggleButton'
 
 function App() {
   return (
     <>
+      <ToggleButton/>
       <Counter/>
     </>
   )
